9a2db541d65e7e7750dc7a3b68fdf18167e0093a
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-04-03 23:45:25 +0530

    Update index.js

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,13 +1,21 @@
+// Write your JS code here
+import {Redirect, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import './index.css'
 
-const NotFound = () => (
-  <div className="not-found-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
-      alt="not found"
-      className="not-found-img"
-    />
-  </div>
-)
-
-export default NotFound
+const Products = props => {
+  if (Cookies.get('jwt_token') === undefined) {
+    return <Redirect to="/login" />
+  }
+  console.log(props)
+  return (
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="product-img"
+      />
+    </div>
+  )
+}
+export default withRouter(Products)
